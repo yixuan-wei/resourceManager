69b685ee1017843bfea956cc2caf2c8673f2a575
yixuanwei <yixuan_wei@qq.com>
2018-10-15 21:45:17 +0800

    inputInit

--- main.cpp
+++ main.cpp
@@ -2,9 +2,18 @@
 #include <string>
 #include <nodes.hpp>
 #include <Node.hpp>
+#include <fstream>
 
 int main(){
-    //TODO 读取resources.txt，建立依赖的dict
+    //TODO 读取resource.txt，建立依赖的dict
+    fstream resourceFile("resource.txt",std::ios::in);
+    if(!resourceFile.is_open()){
+        std::cout<<"Opening resource file failed"<<std::endl;
+    }
+    while(!resourceFile.eof()){
+        std::string dependence;
+        resourceFile.getline()
+    }
     //TODO 建立Node网络，记录网络每一种（个？）资源的dict
     //TODO 实时检查输入
     //TODO 检查删除节点的功能
