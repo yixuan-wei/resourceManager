c335d0ff771d6427999251cb06f1290885b9a5f2
yixuanwei <yixuan_wei@qq.com>
2018-10-25 21:53:04 +0800

    insertFinRestructureInit

--- resourceManager.cpp
+++ resourceManager.cpp
@@ -96,6 +96,7 @@ void PrintInstrHelp() {
 	printf("	insert node n:          insert a node named n into the graph\n");
 	printf("	insert dependence m n:  insert a dependence that m depends on n into the graph\n");
 	printf("	q:                      quit the resource manager\n");
+	printf("    load route_to_log:      load the stored graph and dependences from route_to_log\n");
 	printf("--------------------------\n");
 }
 
@@ -109,23 +110,28 @@ void SplitString(std::string& s, std::list<std::string> &result) {
 	result.push_back(s);
 }
 
-int main(){
-    //读取resource.txt
-	//建立所有依赖的map
-	std::list<Node*> nodes; //store all existing nodes
-	std::map<std::string, std::list<std::string>> depends; //store all the dependences from original txt
+std::string ReplaceString(std::string& s, const char *&from,const char *&to) {
+	//TODO 完成从头到尾检查替换\\&/
+	for (int i = 0; i < s.size(); i++) {
+		if (!strcmp(s.substr(i,strlen(from)).c_str(),from)) {
+			s.replace(i, strlen(from), to);
+			i += strlen(to) - 1;
+		}
+	}
+}
 
+int InitFromResource(std::list<Node*> &nodes, std::map<std::string,std::list<std::string>> &depends) {
 	printf("Read in the resource.txt\n");
-    std::ifstream resourceFile(".\\resource.txt",std::ios::in);
-    if(!resourceFile.is_open()){
-        printf("Opening resource file failed\n");
+	std::ifstream resourceFile(".\\resource.txt", std::ios::in);
+	if (!resourceFile.is_open()) {
+		printf("Opening resource file failed\n");
 		return 1;
-    }
+	}
 	//read in and process the resource file
-    while(!resourceFile.eof()){
+	while (!resourceFile.eof()) {
 		//read in a line from txt
-        std::string dependence;
-        getline(resourceFile, dependence);
+		std::string dependence;
+		getline(resourceFile, dependence);
 		if (dependence == "") break;
 		// cut this line into separate contents
 		std::string dependFrom, dependTo;
@@ -134,11 +140,11 @@ int main(){
 		dependFrom = temp_str.front();
 		dependTo = temp_str.back();
 		temp_str.clear();
-		printf( "%s depends on %s\n", dependFrom.c_str(),dependTo.c_str());
+		printf("%s depends on %s\n", dependFrom.c_str(), dependTo.c_str());
 		//insert dependence into depends
 		auto depend = depends.find(dependFrom);
 		if (depend != depends.end()) {
-			if (std::find(depend->second.begin(),depend->second.end(),dependTo) != depend->second.end()) {
+			if (std::find(depend->second.begin(), depend->second.end(), dependTo) != depend->second.end()) {
 				printf("Dependence from %s to %s already existed\n", dependFrom.c_str(), dependTo.c_str());
 			}
 			else {
@@ -150,25 +156,126 @@ int main(){
 			temp_depend.push_front(dependTo);
 			depends[dependFrom] = temp_depend;
 		}
-		//test if node already exists & create node
-		//TODO 统一成检测并添加的函数模块
-		std::list<Node*>::iterator searchFrom = SearchFromNodes(nodes, dependFrom);
-		if (nodes.empty() || searchFrom == nodes.end()) {
-			Node* temp = new Node(dependFrom.c_str());
-			nodes.push_front(temp);
-			searchFrom = nodes.begin();
-		}
-
-		std::list<Node*>::iterator searchTo = SearchFromNodes(nodes, dependTo);
-		if (searchTo == nodes.end()) {
-			Node* temp = new Node(dependTo.c_str());
-			nodes.push_front(temp);
-			searchTo = nodes.begin();
-		}
-		//update dependence
-		(*searchFrom)->AddDependence(*searchTo);
-    }
+		CreatDependence(nodes, dependFrom, dependTo);
+	}
 	printf("--------Reading Finished---------\n");
+	return 0;
+}
+
+int InitFromLog(std::list<Node*> &nodes, std::map<std::string, std::list<std::string>> &depends, std::string route) {
+	printf("Read in the log file\n");
+	std::ifstream resourceFile(ReplaceString(route,"/","\\"), std::ios::in);
+	if (!resourceFile.is_open()) {
+		printf("Opening log file failed\n");
+		return 1;
+	}
+	//TODO 改成log格式
+	//read in and process the resource file
+	while (!resourceFile.eof()) {
+		//read in a line from txt
+		std::string dependence;
+		getline(resourceFile, dependence);
+		if (dependence == "") break;
+		// cut this line into separate contents
+		std::string dependFrom, dependTo;
+		std::list<std::string> temp_str;
+		SplitString(dependence, temp_str);
+		dependFrom = temp_str.front();
+		dependTo = temp_str.back();
+		temp_str.clear();
+		printf("%s depends on %s\n", dependFrom.c_str(), dependTo.c_str());
+		//insert dependence into depends
+		auto depend = depends.find(dependFrom);
+		if (depend != depends.end()) {
+			if (std::find(depend->second.begin(), depend->second.end(), dependTo) != depend->second.end()) {
+				printf("Dependence from %s to %s already existed\n", dependFrom.c_str(), dependTo.c_str());
+			}
+			else {
+				depend->second.push_front(dependTo);
+			}
+		}
+		else {
+			std::list<std::string> temp_depend;
+			temp_depend.push_front(dependTo);
+			depends[dependFrom] = temp_depend;
+		}
+		CreatDependence(nodes, dependFrom, dependTo);
+	}
+	printf("--------Reading Finished---------\n");
+	return 0;
+}
+
+auto CreatNode(std::list<Node*>&nodes,std::string name) {
+	std::list<Node*>::iterator node = SearchFromNodes(nodes, name);
+	if (nodes.empty() || node == nodes.end()) {
+		Node* temp = new Node(name.c_str());
+		nodes.push_front(temp);
+		node = nodes.begin();
+	}
+	return node;
+}
+
+void CreatDependence(std::list<Node*>&nodes, std::string from, std::string to) {
+	//test if node already exists & create node
+	auto iterFrom = CreatNode(nodes, from);
+	auto iterTo = CreatNode(nodes, to);
+	//update dependence
+	(*iterFrom)->AddDependence(*iterTo);
+}
+
+void DeleteNode(std::list<Node*>&nodes, std::map<std::string, std::list<std::string>> &depends, std::string name) {
+	printf("Going to delete a node\n");
+	std::list<Node*>::iterator target = SearchFromNodes(nodes, name);
+	if (target == nodes.end()) {
+		printf("ERROR: no such node exists in the graph\n");
+	}
+	else {
+		std::string targetName = (*target)->GetName();
+		DeleteFromNodes(nodes, targetName);
+		delete (*target);
+		target = nodes.erase(target);
+		//update whether all the nodes are usable
+		UpdateUsableNodes(nodes, depends);
+	}
+}
+
+void ClearManager(std::list<Node*> &nodes, std::map<std::string, std::list<std::string>>& depends) {
+	// 保存所有node和依赖关系，邻接表
+	std::ofstream outfile("ResourceManagerLog", std::ios::out);
+	outfile << "Graph:" << std::endl;
+	for (auto each = nodes.begin(); each != nodes.end(); each++) {
+		outfile << (*each)->GetName() << " " << (*each)->GetUsable();
+		for (auto depend = (*each)->GetDependencesBegin(); depend != (*each)->GetDependencesEnd(); depend++) {
+			outfile << " " << (*depend)->GetName();
+		}
+		outfile << std::endl;
+	}
+	outfile << "Dependences:" << std::endl;
+	for (auto each = depends.begin(); each != depends.end(); each++) {
+		outfile << each->first << " ";
+		for (auto depend = each->second.begin(); depend != each->second.end(); depend++) {
+			outfile << " " << *depend;
+		}
+		outfile << std::endl;
+	}
+	outfile.close();
+	//删除所有内存（nodes,和里面每一项的所有依赖）
+	for (auto node = nodes.begin(); node != nodes.end();) {
+		delete(*node);
+		node = nodes.erase(node);
+	}
+	nodes.clear();
+	depends.clear();
+}
+
+int main(){
+	std::list<Node*> nodes; //store all existing nodes
+	std::map<std::string, std::list<std::string>> depends; //store all the dependences from original txt
+	//read in the resource.txt and initialize nodes & depends
+	if (InitFromResource(nodes, depends)) {
+		printf("Resource Manager going to abort\n");
+		return 1;
+	}
 	PrintInstrHelp();
 	//TODO examine the input and execute corresponding function all the time
 	while (true) {
@@ -181,68 +288,64 @@ int main(){
 		instruction = temp_instr.front();
 		//delete a node from graph
 		if (!strcmp(instruction.c_str(),"delete")) {
-			printf("Going to delete a node\n");
-			std::list<Node*>::iterator target = SearchFromNodes(nodes, temp_instr.back());
-			if (target == nodes.end()) {
-				printf("ERROR: no such node exists in the graph\n");
-				continue;
+			if (temp_instr.size() == 2) {
+				DeleteNode(nodes, depends, temp_instr.back());
 			}
 			else {
-				std::string targetName = (*target)->GetName();
-				DeleteFromNodes(nodes, targetName);
-				delete (*target);
-				target=nodes.erase(target);
-				//update whether all the nodes are usable
-				UpdateUsableNodes(nodes, depends);
-				continue;
+				printf("ERROR: delete format wrong, please check!\n");
 			}
+			continue;
 		}
 		//insert a node or a dependence into graph
 		else if (!strcmp(instruction.c_str(), "insert")) {
 			temp_instr.pop_front();
 			std::string sub_instr = temp_instr.front();
-			if (sub_instr.c_str() == "node") {
+			if (!strcmp(sub_instr.c_str(),"node") && temp_instr.size()==2) {
 				printf("Going to insert a node\n");
+				CreatNode(nodes, temp_instr.back());
 			}
-			else if (sub_instr.c_str() == "dependence") {
+			else if (!strcmp(sub_instr.c_str(), "dependence") && temp_instr.size()==3) {
+				temp_instr.pop_front();
 				printf("Going to insert a dependence\n");
+				CreatDependence(nodes, temp_instr.front(), temp_instr.back());
+				UpdateUsableNodes(nodes, depends);
 			}
 			else {
-				printf("insert instruction input failed, check format!\n");
+				printf("ERROR: insert instruction input failed, please check format!\n");
 				printf("Going to print help\n");
 			}
+			continue;
 		}
 		else if (!strcmp(instruction.c_str(), "q")) {
 			printf("Ready to quit the manager\n");
-			// 保存所有node和依赖关系，邻接表
-			std::ofstream outfile("ResourceManagerLog", std::ios::out);
-			outfile << "Graph:" << std::endl;
-			for (auto each = nodes.begin(); each != nodes.end(); each++) {
-				outfile << (*each)->GetName() << " " << (*each)->GetUsable();
-				for (auto depend = (*each)->GetDependencesBegin(); depend != (*each)->GetDependencesEnd(); depend++) {
-					outfile << " " << (*depend)->GetName();
-				}
-				outfile << std::endl;
-			}
-			outfile << "Dependences:" << std::endl;
-			for (auto each = depends.begin(); each != depends.end(); each++) {
-				outfile << each->first << " ";
-				for (auto depend = each->second.begin(); depend != each->second.end(); depend++) {
-					outfile << " "<<*depend;
-				}
-				outfile << std::endl;
-			}
-			outfile.close();
-			//删除所有内存（nodes,和里面每一项的所有依赖）
-			for (auto node = nodes.begin(); node != nodes.end();) {
-				delete(*node);
-				node = nodes.erase(node);
-			}
-			nodes.clear();
+			ClearManager(nodes, depends);
 			return 0;
 		}
+		else if (!strcmp(instruction.c_str(), "load")) {
+			if (temp_instr.size() == 2) {
+				printf("Ready to load in log file\n");
+				ClearManager(nodes, depends);
+				if (InitFromLog(nodes, depends, temp_instr.back())) { 
+					printf("Initialization failed, use the older version\n"); 
+				}
+			}
+			else {
+				printf("ERROR: load format wrong, please check!\n");
+				continue;
+			}
+		}
 		else{
-			//TODO 检查并尝试进行节点删除，否则打印帮助
+			// 检查并尝试进行节点删除，否则打印帮助
+			if (temp_instr.size() == 1) {
+				printf("Going to delete this node? y/n\n");
+				char in = 'n';
+				scanf_s("%c", &in);
+				if (in == 'y') DeleteNode(nodes, depends, temp_instr.front());
+				else if (in != 'n') printf("ERROR: wrong input\n");
+			}
+			else {
+				printf("ERROR: wrong input format, please check!\n");
+			}
 			printf("Going to print help\n");
 		}
 	}
